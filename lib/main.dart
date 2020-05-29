7596ecd26bd9af6d480d6ac2f8c4657184b21c0c
Bashiruu1 <silvergold21@gmail.com>
2020-05-28 22:46:38 -0400

    added some buttons and onPressed for button

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,39 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  void answerQuestion() {
+    print("You chose a answer to the question");
+  }
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      "What is your favorite color?",
+      "What is your favroite animal?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Making of My First App"),
-        ),
-        body: Text("This is a body"),
-      ),
+          appBar: AppBar(
+            title: Text("Making of My First App"),
+          ),
+          body: Column(
+            children: [
+              Text("I am a question"),
+              RaisedButton(
+                child: Text("I am answer 1"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("I am answer 2"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("I am answer 3"),
+                onPressed: answerQuestion,
+              )
+            ],
+          )),
     );
   }
 }
